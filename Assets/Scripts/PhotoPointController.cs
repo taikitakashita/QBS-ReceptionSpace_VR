d92bea6b0c98caafc3ccb9a9897f3fff2ee51506
Taiki Takashita <taiki.takashita@qdenbs.com>
2018-11-12 15:55:40 +0900

    撮影ポイントアイコンの拡大縮小、クリック時の色の変更を実装中

---
+++ Assets/Scripts/PhotoPointController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoPointController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Button PhotoPointButton = GetComponent<Button>();    // 対象のボタン
+        PhotoPointButton.animator.SetTrigger("Normal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
