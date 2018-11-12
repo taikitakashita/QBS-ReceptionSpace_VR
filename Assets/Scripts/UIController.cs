56d91e5508896ad8d89b27ada59d27ec9e3621a2
Taiki Takashita <taiki.takashita@qdenbs.com>
2018-11-12 11:28:06 +0900

    レーザーポインターの実装

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,25 +7,30 @@ public class UIController : MonoBehaviour {
     [SerializeField]
     GameObject m_canvas;
 
-    private bool m_canvasStatus;
+    [SerializeField]
+    GameObject m_laserPointer;
+
+    private bool m_UIStatus;
 
     // Use this for initialization
     void Start () {
-        m_canvasStatus = true;
+        m_UIStatus = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.Get(OVRInput.RawButton.Back) && m_canvasStatus == true)
+        if (OVRInput.Get(OVRInput.RawButton.Back) && m_UIStatus == true)
         {
             m_canvas.SetActive(false);
-            m_canvasStatus = false;
+            m_laserPointer.SetActive(false);
+            m_UIStatus = false;
         }
-        else if (OVRInput.Get(OVRInput.RawButton.Back) && m_canvasStatus == false)
+        else if (OVRInput.Get(OVRInput.RawButton.Back) && m_UIStatus == false)
         {
             m_canvas.SetActive(true);
-            m_canvasStatus = true;
+            m_laserPointer.SetActive(true);
+            m_UIStatus = true;
         }
     }
 }
